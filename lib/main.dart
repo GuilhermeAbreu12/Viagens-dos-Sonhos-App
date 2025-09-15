9b9f5c676c8b57a49f2364ad18d6c4de77f247c7
Guilherme Abreu <guilhermesabreu08@gmail.com>
2025-09-14 23:27:12 -0300

    fix: responsive box

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,9 @@ void main() {
 
 class App extends StatelessWidget {
   const App({super.key});
-  
+
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
@@ -24,9 +24,7 @@ class App extends StatelessWidget {
           children: [
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TelaCadastro(),
-              ]
+              children: [TelaCadastro()],
             ),
           ],
         ),
@@ -39,91 +37,115 @@ class TelaCadastro extends StatelessWidget {
   const TelaCadastro({super.key});
   @override
   Widget build(BuildContext context) {
-    return Column (
-      children: [
-        Container(
-          height: MediaQuery.of(context).size.height * 0.3,
-          width: MediaQuery.of(context).size.width * 0.4, // 90% da página
-          
-          decoration: BoxDecoration(
-            border: BoxBorder.all(
-              color: Colors.black, 
-              width: 1,
-            ),
-            borderRadius: BorderRadius.all(
-              Radius.circular(15)
-            ),
-            color: Colors.blue[300]
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.start,
-                
-                children: [
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Container(
-                        height: MediaQuery.of(context).size.height * 0.14,
-                        child: Column(
-                          children: [
-                            SizedBox(
-                              height: 34
-                            ), 
-                            Text(
-                              "Suas viagens dos sonhos são possíveis aqui", 
-                              style: TextStyle(color: Colors.white)
-                            )
-                          ]
-                        )
-                      )
-                    ],
-                  ),
-                ]
-              ),
-              Column(
-                children:[
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      SizedBox(
-                        height: MediaQuery.of(context).size.height * 0.14,
-                        child: Row (
-                          children: [
-                            ElevatedButton.icon(
-                              onPressed: (){},
-                              label: Text("Cadastrar novo"), 
-                              icon: Icon(Icons.add),
-                              style: ElevatedButton.styleFrom(
-                                backgroundColor: Colors.black,
-                                foregroundColor: Colors.white
-                              ), 
-                            ),
-                            ElevatedButton.icon(
-                              onPressed: (){}, 
-                              label: Text("Ver cadastrados"),
-                              icon: Icon(Icons.remove_red_eye_sharp),
-                              style: ElevatedButton.styleFrom(
-                                backgroundColor: Colors.white,
-                                foregroundColor: Colors.black
-                              ),
-                            )
-                          ]
-                        )
+    return ConstrainedBox(
+      // Acabei de aprender isso aqui
+      constraints: BoxConstraints(minHeight: 200, minWidth: 400),
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.black, width: 1),
+          borderRadius: BorderRadius.all(Radius.circular(15)),
+          color: Colors.blue[300],
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.14,
+                      child: Column(
+                        children: [
+                          SizedBox(height: 34, width: 100),
+                          Text(
+                            "Suas viagens dos sonhos são possíveis aqui",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ],
                       ),
-                    ]
-                  )
-                ]
-              )
-            ],
-          )
-        )
-      ]
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.14,
+                      child: Row(
+                        children: [
+                          ElevatedButton.icon(
+                            onPressed: () {},
+                            label: Text("Cadastrar novo"),
+                            icon: Icon(Icons.add),
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.black,
+                              foregroundColor: Colors.white,
+                            ),
+                          ),
+                          ElevatedButton.icon(
+                            onPressed: () {
+                              showDialog(
+                                context: context,
+                                builder: (BuildContext context) {
+                                  return AlertDialog(
+                                    title: Text("Cadastrar"),
+                                    content: Text("Cadastre seu sei lá oq"),
+                                    actions: [Cadastrar()],
+                                  );
+                                },
+                              );
+                            },
+                            label: Text("Ver cadastrados"),
+                            icon: Icon(Icons.remove_red_eye_sharp),
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.white,
+                              foregroundColor: Colors.black,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Cadastrar extends StatelessWidget {
+  const Cadastrar({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
+          child: Text("OI"),
+        ),
+        TextButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          child: Text("Ok"),
+        ),
+      ],
     );
   }
 }
