ff979983639d7778b208fff1aeb2bfe58e250078
Guilherme Abreu <guilhermesabreu08@gmail.com>
2025-09-17 21:14:47 -0300

    feat: add the menu screen system

--- lib/main.dart
+++ lib/main.dart
@@ -37,82 +37,83 @@ class TelaMenu extends StatelessWidget {
   const TelaMenu({super.key});
   @override
   Widget build(BuildContext context) {
-    return ConstrainedBox(
-      // Acabei de aprender isso aqui
-      constraints: BoxConstraints(minHeight: 200, minWidth: 400),
-      child: Container(
-        decoration: BoxDecoration(
-          border: Border.all(color: Colors.black, width: 1),
-          borderRadius: BorderRadius.all(Radius.circular(15)),
-          color: Colors.blue[300],
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.start,
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    SizedBox(
-                      height: MediaQuery.of(context).size.height * 0.14,
-                      child: Column(
-                        children: [
-                          SizedBox(height: 34, width: 100),
-                          Text(
-                            "Suas viagens dos sonhos são possíveis aqui",
-                            style: TextStyle(color: Colors.white),
-                          ),
-                        ],
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.14,
+                  child: Column(
+                    children: [
+                      SizedBox(height: 34, width: 100),
+                      Text(
+                        "Suas viagens dos sonhos são possíveis aqui",
+                        style: TextStyle(color: Colors.white),
                       ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    SizedBox(
-                      height: MediaQuery.of(context).size.height * 0.14,
-                      child: Row(
-                        children: [
-                          ElevatedButton.icon(
-                            onPressed: () {},
-                            label: Text("Cadastrar novo"),
-                            icon: Icon(Icons.add),
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.black,
-                              foregroundColor: Colors.white,
-                            ),
-                          ),
-                          ElevatedButton.icon(
-                            onPressed: () {
-                              TelaCadastro();
-                            },
-                            label: Text("Ver cadastrados"),
-                            icon: Icon(Icons.remove_red_eye_sharp),
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.white,
-                              foregroundColor: Colors.black,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ],
             ),
           ],
         ),
-      ),
+        Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.14,
+                  child: Row(
+                    children: [
+                      ElevatedButton.icon(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => TelaCadastro(),
+                            ),
+                          );
+                        },
+                        label: Text("Cadastrar novo"),
+                        icon: Icon(Icons.add),
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.black,
+                          foregroundColor: Colors.white,
+                        ),
+                      ),
+                      ElevatedButton.icon(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => ConsultarCadastros(),
+                            ),
+                          );
+                        },
+                        label: Text("Ver cadastrados"),
+                        icon: Icon(Icons.remove_red_eye_sharp),
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.white,
+                          foregroundColor: Colors.black,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
